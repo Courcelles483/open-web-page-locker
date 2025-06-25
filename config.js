20cd91b1eb550d812db2e360a118e5bede7991e8
Courcelles483 <158523284+Courcelles483@users.noreply.github.com>
2025-06-25 10:55:00 -0400

    v0.1
    v0.1

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+// Ce fichier définit la configuration, modifie les valeurs ici
+var config = {
+  PASSWORD: "test1234",
+  MAX_ATTEMPTS_SHORT_LOCK: 10,
+  MAX_TOTAL_ATTEMPTS_BAN: 15,
+  SHORT_LOCK_DURATION: 5 * 60 * 1000,      // 5 minutes
+  LONG_LOCK_DURATION: 3 * 60 * 60 * 1000,  // 3 heures
+};
